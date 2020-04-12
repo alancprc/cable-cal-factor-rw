9b54baebe47feecb79e301c1c3d81c4fff21a5a6
alan liang @vapps <alan.liang@xcerra.com>
2020-04-12 16:09:18 +0800

    add test fixture CableLossTest

--- test.cpp
+++ test.cpp
@@ -1,14 +1,19 @@
 #include "cable-cal-factor-rw.h"
 
-#include <boost/property_tree/ptree.hpp>
 #include <iostream>
 #include "gmock/gmock.h"
 
 using namespace std;
 
-TEST(CableLossTest, LoadTest)
+class CableLossTest : public ::testing::Test
 {
+ protected:
   CableLoss cl;
+  void SetUp() {}
+};
+
+TEST_F(CableLossTest, LoadTest)
+{
   cl.load("sample.cf");
 
   EXPECT_EQ(cl.get("RFIN_L_pin", 824e6, 0.0, 1), -36.1);
@@ -16,23 +21,20 @@ TEST(CableLossTest, LoadTest)
   EXPECT_ANY_THROW(cl.get("RFIN_L_pin", 824e6, 0.0, 2));
 }
 
-TEST(CableLossTest, LoadSaveTest)
+TEST_F(CableLossTest, LoadSaveTest)
 {
-  CableLoss cl;
   cl.load("sample.cf");
   cl.save("sample.save");
 }
 
-TEST(CableLossTest, ReadSavedTest)
+TEST_F(CableLossTest, ReadSavedTest)
 {
-  CableLoss cl;
   cl.load("sample.save");
   cl.print();
 }
 
-TEST(CableLossTest, GetSetTest)
+TEST_F(CableLossTest, GetSetTest)
 {
-  CableLoss cl;
   EXPECT_ANY_THROW(cl.get("a", 1, 1, 1));
   cl.set("a", 1, 1, 1, 1.8);
   EXPECT_NO_THROW(cl.get("a", 1, 1, 1));
